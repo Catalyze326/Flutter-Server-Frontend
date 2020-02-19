0a0a1b9ae4ba64916ee2ddaed6429a791706c57d
Catalyze326 <cfmcool@gmail.com>
2020-02-19 14:24:13 -0500

    Added middle server and programs page

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,2 +1,29 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
 List<String> items = List<String>();
 List<String> tempItems = List<String>();
+List<String> devices = List<String>();
+List<String> programs = List<String>();
+Section section;
+
+
+/// Sends the tcp packet to the server and gets the response that has the data
+Future<String> apiRequest(String url, Map jsonMap) async {
+  HttpClient httpClient = new HttpClient();
+  HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
+  request.headers.set('content-type', 'application/json');
+  request.add(utf8.encode(json.encode(jsonMap)));
+  HttpClientResponse response = await request.close();
+  String reply = await response.transform(utf8.decoder).join();
+  httpClient.close();
+  return reply;
+}
+
+enum Section
+{
+  Microcenter,
+  AutoStart,
+  Programs,
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:auto_size_text/auto_size_text.dart';
@@ -11,7 +9,6 @@ import 'globals.dart' as globals;
 
 //TODO fix the links
 //TODO make new things come to the top
-//TODO it auto update
 
 ///runs MyApp and Initalize the code for sending notifications
 void main() {
@@ -37,27 +34,101 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primaryColor: Colors.deepPurple,
       ),
-      home: MyHomePage(title: 'Server Frontend'),
+      home: MainPage(title: 'Server Frontend'),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  MainPage({Key key, this.title}) : super(key: key);
+
+  @override
+  _MainPage createState() => _MainPage();
+}
+
+class _MainPage extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget body;
+    switch (globals.section) {
+      case globals.Section.Microcenter:
+        body = Microcenter();
+        break;
+      case globals.Section.AutoStart:
+        body = AutoStart();
+        break;
+      case globals.Section.Programs:
+        body = Programs();
+        break;
+    }
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.fromLTRB(5, 20, 0, 0),
+          children: <Widget>[
+            ListTile(
+              title: AutoSizeText(
+                "Microcenter",
+                style: TextStyle(fontSize: 23),
+                minFontSize: 8,
+                maxLines: 1,
+              ),
+              onTap: () {
+                setState(() => globals.section = globals.Section.Microcenter);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: AutoSizeText(
+                "AutoStart",
+                style: TextStyle(fontSize: 23),
+                minFontSize: 8,
+                maxLines: 1,
+              ),
+              onTap: () {
+                setState(() => globals.section = globals.Section.AutoStart);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: AutoSizeText(
+                "Programs",
+                style: TextStyle(fontSize: 23),
+                minFontSize: 8,
+                maxLines: 1,
+              ),
+              onTap: () {
+                setState(() => globals.section = globals.Section.Programs);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: body,
+      ),
     );
   }
 }
 
 ///Mezzanine page
-class MyHomePage extends StatefulWidget {
+class Microcenter extends StatefulWidget {
   final String title;
 
-  MyHomePage({Key key, this.title}) : super(key: key);
+  Microcenter({Key key, this.title}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _Microcenter createState() => _Microcenter();
 }
 
 /// Microcenter page
-class _MyHomePageState extends State<MyHomePage> {
+class _Microcenter extends State<Microcenter> {
   Function eq = const ListEquality().equals;
 
-//  List<String> globals.items = List<String>();
-//  List<String> tempglobals.items = List<String>();
   String title;
   RegExp regExp = new RegExp("[0-9]");
   String url = 'http://youcantblock.me';
@@ -69,7 +140,7 @@ class _MyHomePageState extends State<MyHomePage> {
   /// and then send a notification when need be
   getAPIData() async {
     setState(() {
-      apiRequest(url, map).then((s) {
+      globals.apiRequest(url, map).then((s) {
         globals.tempItems = List<String>();
         var parsedJson = json.decode(s);
         for (var item in parsedJson) {
@@ -79,10 +150,10 @@ class _MyHomePageState extends State<MyHomePage> {
             print(val);
           });
           globals.tempItems.add(
-              " 13 : : ______________________________________________________\n");
+              " 13 : : ____________________________________________________\n");
         }
         print(globals.tempItems);
-        if (!eq(globals.tempItems, globals.items)) {
+        if (!eq(globals.tempItems, globals.items) && globals.items.isNotEmpty) {
 //        globals.items = tempglobals.items;
           var players = List<String>();
 //      the emulater
@@ -90,8 +161,7 @@ class _MyHomePageState extends State<MyHomePage> {
 //      my phone
           players.add("f965fbba-e53f-406e-b5c4-ee0aa5edd948");
           var notification = OSCreateNotification(
-              playerIds: players,
-              content: "There are new microcenter globals.items");
+              playerIds: players, content: "There are new microcenter items");
           OneSignal().postNotification(notification);
         }
         globals.items = globals.tempItems;
@@ -99,40 +169,10 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  /// Sends the tcp packet to the server and gets the response that has the data
-  Future<String> apiRequest(String url, Map jsonMap) async {
-    HttpClient httpClient = new HttpClient();
-    HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
-    request.headers.set('content-type', 'application/json');
-    request.add(utf8.encode(json.encode(jsonMap)));
-    HttpClientResponse response = await request.close();
-    String reply = await response.transform(utf8.decoder).join();
-    httpClient.close();
-    return reply;
-  }
-
   @override
   Widget build(BuildContext context) {
     getAPIData();
-//    while(globals.items.isEmpty)sleep(const Duration(milliseconds: 10));
     return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            ListTile(
-              title: AutoSizeText(
-                "Microcenter",
-                style: TextStyle(fontSize: 23),
-                minFontSize: 8,
-                maxLines: 2,
-              ),
-              onTap: () {},
-            )
-          ],
-        ),
-      ),
       body: Padding(
         padding: EdgeInsets.all(15.0),
         child: ListView.builder(
@@ -160,8 +200,170 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: getAPIData,
         tooltip: 'send',
-        child: Icon(Icons.add),
+        child: Icon(Icons.get_app),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AutoStart extends StatefulWidget {
+  final String title;
+
+  AutoStart({Key key, this.title}) : super(key: key);
+
+  @override
+  _AutoStart createState() => _AutoStart();
+}
+
+class _AutoStart extends State<AutoStart> {
+  RegExp regExp = new RegExp("[0-9]");
+
+  String url = 'http://youcantblock.me';
+
+  getAllDevices() async {
+    setState(() {
+      globals.apiRequest(url, {
+        "action": {"devices": "update"}
+      }).then((s) {
+        s = s.substring(10, s.length - 2);
+        globals.devices = List<String>();
+        var devices = s.split("*");
+        for (var i = 0; i < devices.length; i++) {
+          if (i & 1 == 1) globals.devices.add(devices[i]);
+        }
+      });
+      print(globals.devices);
+    });
+  }
+
+  sendRequest(Map map) {
+    globals.apiRequest(url, map);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: globals.devices.length != 0
+            ? ListView.builder(
+                itemCount: globals.devices.length,
+                itemBuilder: (context, index) {
+                  return (Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      AutoSizeText(
+                        globals.devices[index],
+                        style: TextStyle(fontSize: 32),
+                        minFontSize: 15,
+                        maxLines: 1,
+                      ),
+                      FloatingActionButton(
+                        onPressed: sendRequest(Map.of({
+                          "action": {
+                            "devices": {globals.devices[index]: "togglePower"}
+                          }
+                        })),
+                        tooltip: 'togglePower',
+                        child: Icon(Icons.power),
+                      ),
+                    ],
+                  ));
+                })
+            : AutoSizeText(
+                "No devices yet",
+                style: TextStyle(fontSize: 18),
+                minFontSize: 8,
+                maxLines: 2,
+              ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getAllDevices,
+        tooltip: 'send',
+        child: Icon(Icons.get_app),
+      ),
+    );
+  }
+}
+
+class Programs extends StatefulWidget {
+  final String title;
+
+  Programs({Key key, this.title}) : super(key: key);
+
+  @override
+  _Programs createState() => _Programs();
+}
+
+class _Programs extends State<Programs> {
+  RegExp regExp = new RegExp("[0-9]");
+
+  String url = 'http://youcantblock.me';
+
+  getAllPrograms() async {
+    setState(() {
+      globals.apiRequest(url, {
+        "action": {"programs": "update"}
+      }).then((s) {
+        s = s.replaceAll(RegExp("[^a-zA-Z0-9,.:]"), "");
+        print(s);
+        globals.programs = s.split(RegExp("[,:]"));
+        print(globals.programs);
+      });
+      print(globals.programs);
+    });
+  }
+
+  sendRequest(Map map) {
+    globals.apiRequest(url, map);
+  }
+
+  Widget returnText(String s) {
+    try {
+      if (int.parse(s.split(".")[0]) >
+          new DateTime.now().millisecondsSinceEpoch - (60 * 1000 * 2)) {
+        return AutoSizeText("Running\n",
+            style: TextStyle(color: Colors.green, fontSize: 32));
+      } else {
+        return AutoSizeText("Stalled\n",
+            style: TextStyle(color: Colors.red, fontSize: 32));
+      }
+    } catch (ignore) {
+      return AutoSizeText(s,
+          style: TextStyle(fontSize: 32), minFontSize: 15, maxLines: 1);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getAllPrograms();
+    print(globals.programs);
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: globals.programs.length != 0
+            ? ListView.builder(
+                itemCount: globals.programs.length,
+                itemBuilder: (context, index) {
+                  return (Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      returnText(globals.programs[index]),
+                    ],
+                  ));
+                })
+            : AutoSizeText(
+                "No programs yet",
+                style: TextStyle(fontSize: 18),
+                minFontSize: 8,
+                maxLines: 2,
+              ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getAllPrograms,
+        tooltip: 'send',
+        child: Icon(Icons.get_app),
+      ),
     );
   }
 }
